4c3b445d763c6fe947b046eb34ca99099b4e96dd
sahoffma <shoffman8470@gmail.com>
2013-10-30 01:36:31 -0400

    Added express-partials support for server(better server-side templating)
    -Removed most static HTML pages and replaced with templated .ejs versions

---
+++ 3D-Monopoly/routes/mainpage.js
@@ -0,0 +1,48 @@
+
+/*
+ * GET home page.
+ */
+
+exports.login = function(req, res){
+	if(!req.session){
+		res.redirect('/home/')
+	}
+	else{
+  res.render('login.ejs', { layout: 'mainpagelayout', title: 'Login' });
+   }
+};
+
+exports.contact = function(req, res){
+	if(!req.session){
+		res.redirect('/home/')
+	}
+	else{
+  res.render('contact.ejs', { layout: 'mainpagelayout', title: 'Contact' });
+   }
+};
+
+exports.register = function(req, res){
+	if(!req.session){
+		res.redirect('/home/')
+	}
+	else{
+  res.render('register.ejs', { layout: 'mainpagelayout', title: 'Register' });
+   }
+};
+
+exports.screenshots = function(req, res){
+	if(!req.session){
+		res.redirect('/home/')
+	}
+	else{
+  res.render('screenshots.ejs', { layout: 'mainpagelayout', title: 'Screenshots' });
+   }
+};
+exports.hub = function(req, res){
+    if(!req.session){
+	res.redirect('/home');
+    }
+    else{
+	res.render('hub', {title: '3D Monopoly' });
+    }
+};
